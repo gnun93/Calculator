c30234dadc46007b5d637d95b9d98ac79c851d42
gnun93 <gnun93@naver.com>
2020-09-14 22:15:46 +0900

    Initial Commit

---
+++ calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calc
+//
+//  Created by 장진원 on 2020/09/14.
+//  Copyright © 2020 Jin won Jang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
